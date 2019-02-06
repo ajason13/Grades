e51a2fd77f58f5d6d5124ee01871f432525a1469
Jason Alvarez <a.jason13@gmail.com>
2019-02-05 19:49:27 -0800

    Added statistics calculations

---
+++ Grades/GradeStatistics.cs
@@ -0,0 +1,16 @@
+﻿namespace Grades
+{
+    class GradeStatistics
+    {
+        public GradeStatistics()
+        {
+            HighestGrade = 0;
+            LowestGrade = float.MaxValue;
+        }
+
+        // Holding Alt + Click = free form select
+        public float AverageGrade;
+        public float HighestGrade;
+        public float LowestGrade;
+    }
+}
