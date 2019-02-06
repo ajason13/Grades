6d2e28e07b5a0cdf9cbf5b67169965dbe5471249
Jason Alvarez <a.jason13@gmail.com>
2019-02-05 19:16:26 -0800

    First Commit

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        List<float> grades;
+
+        // ctor + 2x tab = constructor
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        
+    }
+}
